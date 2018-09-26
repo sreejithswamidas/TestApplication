54c474712036c2b484265bfcab34f08fdf9e8701
sreejith.swamidas <sreejith.swamidas@gmail.com>
2018-09-26 16:11:19 +0530

    Using Stream processing

--- src/main/java/tcl/org/test/TransactionList.java
+++ src/main/java/tcl/org/test/TransactionList.java
@@ -71,7 +71,43 @@ public class TransactionList {
         return statisticsModel;
 
         }
+    //Set statistics model values
+    /*public StatisticsModel setStatisticsModel(StatisticsModel statisticsModel)
+    {
+        count=0;
+        total=0;
+        average=0;
+        max=0;
+        min=100;
+        boolean nocontent=true;
+        for(TransactionModel transaction:transactions){
+            float amount=transaction.getAmount();
+            if(checkTimeStamp(transaction.getTimestamp())) {
+                nocontent=false;
+                count++;
+                total += transaction.getAmount();
+                average = total / count;
+                if (amount > max)
+                    max = amount;
+                if (amount < min)
+                    min = amount;
+            }
+        }
 
+        statisticsModel.setCount(count);
+        statisticsModel.setTotal(setDecimalPoints(total));
+        statisticsModel.setAverage(setDecimalPoints(average));
+        statisticsModel.setMax(max);
+        //if no transaction i last 60 seconds
+        if(nocontent){
+            statisticsModel.setMin(0);
+        }
+        else
+        statisticsModel.setMin(min);
+        set();
+        return statisticsModel;
+    }
+*/
    }
 
 
